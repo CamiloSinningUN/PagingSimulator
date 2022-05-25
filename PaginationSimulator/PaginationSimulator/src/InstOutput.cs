9775b541198731955f32308865a6fe53260ecbe9
Leonardo Vergara <leonardo11vergara11@gmail.com>
2022-05-25 16:39:58 -0500

    Clase InstOutput añadida

---
+++ PaginationSimulator/PaginationSimulator/src/InstOutput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaginationSimulator.src
+{
+    public class InstOutput
+    {
+        public InstOutput(int dirLog, int dirFis, bool lec, int pag, int marco, bool swapIn, bool swapOut, string bitac)
+        {
+            this.dirLog = dirLog;
+            this.dirFis = dirFis;
+            this.lec = lec;
+            this.pag = pag;
+            this.marco = marco;
+            this.swapIn = swapIn;
+            this.swapOut = swapOut;
+            this.bitac = bitac;
+        }
+
+        public int dirLog;
+        public int dirFis;
+        public bool lec;
+        public int pag;
+        public int marco;
+        public bool swapIn;
+        public bool swapOut;
+        public string bitac;
+    }
+}
